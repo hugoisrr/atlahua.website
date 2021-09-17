a8dbed657b0835df6d9a2e7cdbf8d6a872bc02e4
Hugo Israel Ramirez Soto <84rami@gmail.com>
2021-09-17 14:28:18 +0200

    modified images path

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: 'Demonstrator',
     description:
       'The main purpose of this sub-system is to visualize the current process of a machine and through graphs visualize the stream of data coming from the sensors that are located on/in machines.',
-    image: '/atlahua.website/images/Demonstrator.jpg',
+    image: '/images/Demonstrator.jpg',
     tags: ['Socketio', 'Redux', 'React', 'Sass', 'D3', 'Victory'],
     source: 'https://google.com',
     visit: 'https://github.com/hugoisrr/demonstrator',
@@ -13,7 +13,7 @@ export const projects = [
     title: 'MoCo-Testprotokoll',
     description:
       'Through a web server that communicates to a React web app that uses Context as state management and REST API, stores data of test protocols with MongoDB. Then the tests are store in text files.',
-    image: '/atlahua.website/images/MoCoTestprotokoll.jpg',
+    image: '/images/MoCoTestprotokoll.jpg',
     tags: ['React', 'Node', 'JSON', 'Bootstrap', 'MongoDB'],
     source: 'https://google.com',
     visit: 'https://github.com/hugoisrr/moco-testprotokoll',
@@ -23,7 +23,7 @@ export const projects = [
     title: 'MeetUp',
     description:
       "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-    image: '/atlahua.website/images/MeetUp.jpg',
+    image: '/images/MeetUp.jpg',
     tags: ['React', 'Node', 'Express', 'Sass', 'MongoDB'],
     source: 'https://google.com',
     visit: 'https://github.com/hugoisrr/meetup',
@@ -33,7 +33,7 @@ export const projects = [
     title: 'LeanDA',
     description:
       'IIoT solution currently develop at Fraunhofer IPA. This system is intended to process and support real time business systems and alerts. It manages locally the data sent by sensors, PLCs, and tablets.',
-    image: '/atlahua.website/images/LeanDA.jpg',
+    image: '/images/LeanDA.jpg',
     tags: ['Express', 'Node', 'JWT', 'Websocket', 'MongoDB', 'JSON'],
     source: 'https://google.com',
     visit: 'https://github.com/hugoisrr/leanda-system',
